35b66ada400f9874dcebddd016a6ee8892008877
Mehmet Kaya <mehmet.kaya12@hacettepe.edu.tr>
2016-01-12 18:14:08 +0200

    Deneme commit

--- app/src/main/java/me/mehmetkaya/eldevar/MainActivity.java
+++ app/src/main/java/me/mehmetkaya/eldevar/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements TokenCompleteText
 
     LinearLayout horizontalContainer;
     int ilkEfekt;
-    int asd;
+    int asdq;
     int efektFadeOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
